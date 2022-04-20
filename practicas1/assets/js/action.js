fe87949f4172acaed6ce3baee5ed27437a35d3a1
kellyurdaneta <kellyurdanetar@gmail.com>
2022-04-20 11:22:29 -0300

    script agregado

---
+++ practicas1/assets/js/action.js
@@ -0,0 +1,9 @@
+/*Iniciamos las variables*/
+let nombre = prompt("Ingrese su nombre ");
+let apellido = prompt("Ingrese su apellido ");
+console.log("Su nombre y apellido es " + nombre + " " + apellido); 
+alert("Su nombre y apellido es " + nombre + " " + apellido);
+
+
+
+
